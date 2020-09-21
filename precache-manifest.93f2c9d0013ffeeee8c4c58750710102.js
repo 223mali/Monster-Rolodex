2816ecf7d6114f2b859a6ca5c88ba4dda9e0ed76
keita ernest <keitaernest@gmail.com>
2020-09-21 22:52:30 +0000

    Updates

---
+++ precache-manifest.93f2c9d0013ffeeee8c4c58750710102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acd0aaa745d0a4620a01d3b2196447a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8184c33d95cbef50e9cb",
+    "url": "/monsters-rolodex/static/css/main.2416a2f7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8184c33d95cbef50e9cb",
+    "url": "/monsters-rolodex/static/js/main.3635eb64.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
